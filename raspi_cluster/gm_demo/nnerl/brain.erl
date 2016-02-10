e991d10831e4834d6bb1872bc3858cff8b790ed7
philkuz <superpker@gmail.com>
2016-02-09 16:40:24 -0800

    made connections

--- raspi_cluster/gm_demo/nnerl/brain.erl
+++ raspi_cluster/gm_demo/nnerl/brain.erl
@@ -1,20 +1,69 @@
 -module(brain).
--export([construct/3, access/3, get_adjacents/3, matrix/1, ncols/1, nrows/1]).
+-export([brain/0, brain/1,brain/3, construct/3, main/0]).
 
--record(matrix, {matrix=[], r=0, c=0}).
-
-
-
-% floor(X) when X < 0 ->
-%     T = trunc(X),
-%     case X - T == 0 of
-%         true -> T;
-%         false -> T - 1
-%     end;
-% floor(X) ->
-%     trunc(X).
+-record(matrix, {matrix=[],
+                r=0,
+                c=0}).
+-record(brainParams, {  ins=[],
+                        outs=[],
+                        nodesz=0,
+                        inputsz=0,
+                        outputsz=0
+                    }).
 
 
+% the main server
+brain() ->
+    brain(#brainParams{}).
+brain(Nodesz, Inputsz, Outputsz) ->
+    brain(#brainParams{nodesz=Nodesz, inputsz=Inputsz, outputsz=Outputsz}).
+brain(Params) ->
+    % io:format('~w~n', ['lit']),
+    receive
+        newbrain ->
+            NewParams = construct(Params),
+            brain(NewParams);
+        {feed, Inputs} ->
+            feed(Params#brainParams.ins, Inputs),
+            brain(Params);
+        % returns a list of the output neuron values
+        getoutput ->
+            % [Head | Tail] = Params#brainParams.outs,
+            % io:format("slit~w~n", [neuron:net(Head)]),
+            printNets(Params#brainParams.outs),
+            % [neuron:net(X  ! getNeuron) || X <- Params#brainParams.outs],
+            brain(Params)
+    end.
+printNets([]) ->
+    ok;
+printNets(L) ->
+    [Head | Rest] = L,
+    Head ! printNet,
+    % io:format("this~w~n",[(Head ! getNeuron)]),
+    printNets(Rest).
+% feeds the network input values
+feed([],_) ->
+    ok;
+feed(_,[]) ->
+    ok;
+feed(InputPIDs, InputVals) ->
+    [PID| RestPID] = InputPIDs,
+    [Val | RestVal] = InputVals,
+    PID ! {feed, Val},
+    feed(RestPID, RestVal).
+
+
+% function which rounds a number to the nearest int <= x
+floor(X) when X < 0 ->
+    T = trunc(X),
+    case X - T == 0 of
+        true -> T;
+        false -> T - 1
+    end;
+floor(X) ->
+    trunc(X).
+
+% function which rounds a number to the nearest int >= x
 ceiling(X) when X < 0 ->
     trunc(X);
 ceiling(X) ->
@@ -58,15 +107,66 @@ access(MatrixRec, R, C) ->
         mod(C, ncols(MatrixRec))+1,
         lists:nth(mod(R, nrows(MatrixRec))+1, Matrix)).
 
-% Creates a graph that matches the nodal structure
+% returns random values
+get_ins_outs(InputC, OutputC, MatrixRec) ->
+    L = lists:seq(1, trunc(math:pow(ncols(MatrixRec),2))),
+    Shuffle = [X||{_,X} <- lists:sort([ {random:uniform(), N} || N <- L])],
+    Ins = getPids(lists:sublist(Shuffle, InputC),[], MatrixRec),
+    Outs = getPids(output,lists:sublist(Shuffle, InputC+1, OutputC),[], MatrixRec),
+    {Ins, Outs}.
+
+% converts the position value from a 1d vector to a 2d of dimensions MxN
+dto2d(Pos, M, N) ->
+    X = floor(Pos/M),
+    Y = Pos rem N,
+    {X,Y}.
+
+% returns the Pids of all
+getPids([], OutputL, _) ->
+    OutputL;
+getPids(InputL, OutputL, MatrixRec) ->
+    [FirstI| RestI] = InputL,
+    {X,Y} = dto2d(FirstI, nrows(MatrixRec), ncols(MatrixRec)),
+    getPids(RestI, [access(MatrixRec, X, Y)|OutputL], MatrixRec).
+
+% returns the Pids of all and marks some as output
+getPids(output, [], OutputL, _) ->
+    OutputL;
+getPids(output,InputL, OutputL, MatrixRec) ->
+    [FirstI| RestI] = InputL,
+    {X,Y} = dto2d(FirstI, nrows(MatrixRec), ncols(MatrixRec)),
+    NewPid = access(MatrixRec, X, Y),
+    NewPid ! output,
+    getPids(output, RestI, [NewPid| OutputL], MatrixRec).
+% % Creates a graph that matches the nodal structure
+construct(BrainParams) ->
+    #brainParams{inputsz=InputC, outputsz=OutputC, nodesz=NodeC} = BrainParams,
+    construct(NodeC, InputC, OutputC).
 construct(NodeC, InputC, OutputC) ->
+    % #brainParams{inputsz=InputC, outputsz=OutputC, nodesz=NodeC} = BrainParams,
     Side = ceiling(math:sqrt(NodeC)),
+    BrainParams = #brainParams{inputsz=InputC, outputsz=OutputC, nodesz=Side*Side},
+
     ProxMat = gen_matrix(Side,Side),
     % Matrix = matrix(ProxMat),
-    % setup adjacency
-    proximity_rows(matrix(ProxMat), Side, Side, ProxMat)
-    % pull out the input and output matrices
-    .
+    % setup proximity graph
+    io:format("pre-proximity~n",[]),
+    proximity_rows(matrix(ProxMat), Side, Side, ProxMat),
+    io:format("post-proximity~n",[]),
+    % pull out the input and output lists
+    {Ins, Outs} = get_ins_outs(InputC, OutputC, ProxMat),
+    % Construct the Connectivity graph
+    connectivity(Ins, 0),
+    % io:format("post-connectivity~n",[]),
+    BrainParams#brainParams{ins=Ins, outs=Outs}.
+
+% handles the dfs connectivity
+connectivity([], _) ->
+    ok;
+connectivity(InputNL, Counter) ->
+    [Head | Rest] = InputNL,
+    Head ! {startdfs, Counter},
+    connectivity(Rest, Counter+1).
 
 % column-wise adjacency adjustment
 proximity_rows([],_,_, _) ->
@@ -105,8 +205,13 @@ get_adjacents(MatrixRec, R, C) ->
     access(MatrixRec, R-1, C-1),
     access(MatrixRec, R, C-1),
     access(MatrixRec, R+1, C-1)].
-
-
+% c(brain) and c(neuron) first
+main() ->
+    % c(neuron),
+    % unregister(brain),
+    register(brain, spawn(brain, brain, [200000,8,4])),
+    brain ! newbrain,
+    brain ! getoutput.
 
 
 
--- raspi_cluster/gm_demo/nnerl/neuron.erl
+++ raspi_cluster/gm_demo/nnerl/neuron.erl
@@ -1,15 +1,28 @@
 -module(neuron).
--export([neuron/5, neuron/0]).
+-export([neuron/1, neuron/0, net/1]).
 
-random() ->
-    0.
+-record(neuron, {proximals=[],
+                posteriors=[],
+                weights=[],
+                net=0,
+                marker=-1,
+                markerProx=[],
+                output=false}). % the proximals that have not been checked by posteriors
+
+%  Neuron **SHOULD BE**  a neuron, but is not
+net(Neuron) ->
+    Neuron#neuron.net.
+% shuffle a list randomly
+shuffle(L) ->
+    [X||{_,X} <- lists:sort([ {random:uniform(), N} || N <- L])].
+
 % checks whether an element is in a list
 listFind(Element, List) ->
     lists:member(Element, List).
 
  % added this line because of vague notion of erlang proper form
 thresholding(Posteriors, Weights, NewNet) ->
-    Threshold = 10,
+    Threshold = 0.5,
     if
         NewNet > Threshold ->
             feed(Posteriors, Weights, NewNet),
@@ -18,66 +31,91 @@ thresholding(Posteriors, Weights, NewNet) ->
             NewNet
     end.
 
-
-
-% global variable holder
-% commmented out because of proper erlang syntax
-% threshold() ->
-%     10.
-
-
 % the neuron process handler
 % neuron([], [], [], 0, -1)
-neuron() ->
-    neuron([], [], [], 0, -1).
-neuron(Proximals, Posteriors, Weights, Net, Marker) ->
-    receive
-        {feed, Num} ->
-            neuron(Proximals, Posteriors, Weights, thresholding(Posteriors, Weights, Num + Net), Marker);
-            % replaced code block because of erlang standards
-            % NewNet = Num + Net,
-            % if
-            %     Num + Net > threshold() ->
-            %         feed(Posterior, Weights, Num+Net),
-            %         neuron(Proximals, Posteriors, Weights, 0);
-            %     true ->
-            %         neuron(Proximals, Posteriors, Weights, Net+Num)
-            % end;
+addProximal(Neuron, Proximal) ->
+    Proximals = Neuron#neuron.proximals,
+    Neuron#neuron{proximals=[Proximal|Proximals]}.
+addPosterior(Neuron, Posterior) ->
+    Posteriors = Neuron#neuron.posteriors,
+    case listFind(Posterior, Posteriors) of
+        false ->
+            Neuron#neuron{posteriors=[Posterior|Posteriors], weights=[min(random:uniform(), 0.99) |  Neuron#neuron.weights]};
+        true ->
+            Neuron
+    end.
 
-        {proximal, Neuron} ->
-            neuron([Neuron | Proximals], Posteriors, Weights, Net, Marker);
-        % makes a posterior connection with the neuron
-        % self() -> Neuron
-        {posterior, Neuron} ->
-            % run a check
-            case listFind(Neuron, Posteriors) of
-                false ->
-                    neuron(Proximals, [Neuron | Posteriors], [random() | Weights], Net, Marker);
-                true ->
-                    neuron(Proximals, Posteriors, Weights, Net, Marker)
-            end;
-        {dfs, NewMarker, Parent} ->
+neuron() ->
+    neuron(#neuron{}).
+% neuron(Proximals, Posteriors, Weights, Net, Marker) ->
+neuron(Neuron) ->
+    receive
+        % feed forward structure
+        printNet ->
+            io:format("Net: ~w~n", [net(Neuron)]),
+            neuron(Neuron);
+        {feed, Num} ->
+            Posteriors = Neuron#neuron.posteriors,
+            Weights = Neuron#neuron.weights,
+            Net = Neuron#neuron.net,
+            %io:format("{~w, Net:~w}~n",[self(),Net+Num]),
+            neuron(Neuron#neuron{net=thresholding(Posteriors, Weights, Num+Net)});
+        % adds a proximal node to this neuron's graph
+        {proximal, Prox} ->
+            neuron(addProximal(Neuron, Prox));
+
+        % makes a posterior connection with the neuron. If it already exists, then
+        % nothing happens
+        {posterior, Posterior} ->
+            neuron(addPosterior(Neuron, Posterior));
+        % if the dfs of as child node fails, then
+        {faileddfs, Touched, Marker} ->
+            MarkerProximals= Neuron#neuron.markerProx,
+            NewNeuron = Neuron#neuron{markerProx=lists:delete(Touched, MarkerProximals)},
+            dfsNext(NewNeuron, Marker, self()),
+            neuron(NewNeuron);
+        {startdfs, NewMarker} ->
+            dfsNext(resetDFS(Neuron), NewMarker, self()),
+            neuron(Neuron);
+        % the dfs receiver
+        {dfs, NewMarker, ParentPID} ->
+            #neuron{marker=Marker, output=IsOutput} = Neuron,
             if
+                % stop running dfs if we hit output node
+                IsOutput == true ->
+                    ParentPID ! {posterior, self()},
+                    neuron(Neuron);
+                % has not been marked by this node yet
                 NewMarker > Marker ->
-                    Parent ! {posterior, self()},
-                    dfs(Proximals, NewMarker, self()),
-                    neuron(Proximals, Posteriors, Weights, Net, NewMarker)
-            end
+                    % notify the parent that this is a good connection
+                    ParentPID ! {posterior, self()},
+                    % reset the MarkProximals s othat they will randomly choose
+                    dfsNext(resetDFS(Neuron), NewMarker, self()),
+                    neuron(Neuron);
+                %  has been marked
+                true ->
+                    ParentPID ! {faileddfs, self(), NewMarker},
+                    neuron(Neuron)
+            end;
+        % flags this node as output
+        output ->
+            neuron(Neuron#neuron{output=true})
 
     end.
-% depth first search along the proximals
-dfs([], _, _) ->
+% reset the MarkProximals s othat they will randomly choose
+resetDFS(Neuron) ->
+    MarkerProx = shuffle(Neuron#neuron.proximals),
+    Neuron#neuron{markerProx=MarkerProx}.
+dfsNext(#neuron{markerProx=[]}, _, _) ->
     ok;
-dfs(Proximals, NewMarker, Self) ->
-    [Head|Rest] = Proximals,
-    Head ! {dfs, NewMarker, Self},
-    dfs(Rest, NewMarker, Self).
+dfsNext(#neuron{markerProx=Proximals}, Marker, ParentPID) ->
+    [Head | _] = Proximals,
+    Head ! {dfs, Marker, ParentPID}.
 
 % handles feed forwarding for a list of neurons and their corresponding weights
 feed([], [], _) ->
     ok;
 feed(Neurons, Weights, Net) ->
-    io:format('done'),
     [NextNeur | RestNeurs]  = Neurons,
     [NextWeight | RestWeight] = Weights,
     NextNeur ! {feed, NextWeight * Net},
