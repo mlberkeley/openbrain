d11b3a54de1af1123efc26f972a0b30aabe84e96
Utkarsh Singhal <s.utkarsh@berkeley.edu>
2016-04-19 22:06:09 -0700

    shitty boltzmann agent initial implementation

--- pacman_demo/pacmanAgents.py
+++ pacman_demo/pacmanAgents.py
@@ -17,11 +17,15 @@ from game import Agent
 import random
 import game
 import util
+import numpy as np
+import scipy.special import expit
 
 class LeftTurnAgent(game.Agent):
     "An agent that turns left at every opportunity"
 
     def getAction(self, state):
+        
+
         legal = state.getLegalPacmanActions()
         current = state.getPacmanState().configuration.direction
         if current == Directions.STOP: current = Directions.NORTH
@@ -50,3 +54,59 @@ class GreedyAgent(Agent):
 
 def scoreEvaluation(state):
     return state.getScore()
+
+
+class OpenBrainAgent(Agent):
+    def __init__(self, evalFn="scoreEvaluation", num_neurons=1000 ):
+        self.num_inputs = 100 #TODO make sure corrrect.
+        self.num_outputs = 4
+        total_neurons = num_neurons+num_inputs+num_outputs
+        self.W = np.random.random((total_neurons, total_neurons))
+        self.W *= 1 - np.eye((total_neurons, total_neurons)) #delete self connections
+        self.W *= 1- np.outer( (np.arange(total_neurons)>=(total_neurons-num_outputs))*1, (np.arange(total_neurons)>=(total_neurons-num_outputs))*1)
+        self.decay_const = 0.8
+
+        #Todo gaussian
+        self.rho = np.zeros((total_neurons,))
+        self.rho_naught = np.ones((total_neurons,))
+        self.rho_naught *= (np.arange(total_neurons) > )*1
+        self.R = np.ones((total_neurons,))
+        self.v = np.zeros((total_neurons,)) #Voltages
+        self.activation = expit
+
+    def get_outputs(self):
+        return self.v[:-self.num_outputs]
+    def set_inputs(self, i):
+        self.v[:self.num_inputs] = i #TODO stochastic. 
+    
+    def get_inputs(self, state):
+        pass
+
+    def update(self,input_state):
+        self.v *= self.decay_const
+        self.set_inputs(input_state))
+        delta_v = self.activation(W.dot(R*self.v))
+        self.v = self.v + delta_v - (self.v*self.R)
+        self.rho -= (1*(self.rho != 0)) - self.rho_naught*self.R
+        self.R = (self.rho == 0)*1
+
+    def learn(self,state):
+        pass
+
+    def getAction(self, state):
+        incoming_inputs = self.get_inputs(state)
+        self.update(self,incoming_inputs)
+        self.learn(self,state)
+        out_vect = self.get_outputs()
+        output = fhoaifhas (out_vect)
+        return 
+
+        # Generate candidate actions
+        # legal = state.getLegalPacmanActions()
+        # if Directions.STOP in legal: legal.remove(Directions.STOP)
+
+        # successors = [(state.generateSuccessor(0, action), action) for action in legal]
+        # scored = [(self.evaluationFunction(state), action) for state, action in successors]
+        # bestScore = max(scored)[0]
+        # bestActions = [pair[1] for pair in scored if pair[0] == bestScore]
+        # return random.choice(bestActions)
