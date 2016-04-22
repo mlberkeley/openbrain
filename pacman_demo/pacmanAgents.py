e75a183cd9250a1ac10e73fcf8718405969e3a6a
philkuz <superpker@gmail.com>
2016-04-22 16:45:21 -0700

    Played around with pacman agent and added point-watching into the reinforcement rules.

--- pacman_demo/pacmanAgents.py
+++ pacman_demo/pacmanAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -36,63 +36,63 @@ class _OpenBrainInput():
         self._last_pacman_coord = None
         self._last_ghost_coords = None
         self._last_foods = None
-        
+
     def get_vec(self):
         return self.input_vec.copy()
-        
+
     @staticmethod
     def _grid_to_matrix(grid):
         n, m = grid.height, grid.width
         matrix = np.zeros([n, m])
         for x, y in grid.asList():
             matrix[n-y-1][x] = 1
-        return matrix 
-        
+        return matrix
+
     def _pos_to_coord(self, pos):
         return self.n - pos[1], pos[0]
-        
+
     def _init_input_vec(self, state):
         walls_mat = _OpenBrainInput._grid_to_matrix(state.getWalls())
         foods_mat = _OpenBrainInput._grid_to_matrix(state.getFood())
-        
+
         self.n, self.m = walls_mat.shape
-        
+
         pacman_mat = np.zeros([self.n ,self.m])
         pacman_pos = state.getPacmanPosition()
         self._last_pacman_coord = self._pos_to_coord(pacman_pos)
         pacman_mat[self._last_pacman_coord] = 1
-        
+
         ghosts_mat = np.zeros([self.n, self.m])
         ghost_poss = state.getGhostPositions()
         self._last_ghost_coords = [self._pos_to_coord(pos) for pos in ghost_poss]
         for coord in self._last_ghost_coords:
             ghosts_mat[coord] = 1
-        
+
         large_mat = np.r_[walls_mat, np.r_[foods_mat, np.r_[pacman_mat, ghosts_mat]]]
         self.input_vec = large_mat.ravel()
-        
+
         self._last_foods = state.getFood().copy()
-                    
+
     def _flat_coord(self, k, coord):
         return self.n * self.m * k + self.m * coord[0] + coord[1]
-                    
+
     def update(self, state):
         if self.input_vec is None:
             self._init_input_vec(state)
         else:
             pacman_pos = state.getPacmanPosition()
             pacman_coord = self._pos_to_coord(pacman_pos)
-            
-            #update foods 
+
+            #update foods
             if self._last_foods[pacman_pos[0]][pacman_pos[1]]:
                 self.input_vec[self._flat_coord(1, pacman_coord)] = 0
                 self._last_foods = state.getFood().copy()
-                
+
             #update pacman
             self.input_vec[self._flat_coord(2, self._last_pacman_coord)] = 0
             self.input_vec[self._flat_coord(2, pacman_coord)] = 1
             self._last_pacman_coord = pacman_coord
-            
+
             #update ghosts
             for ghost_coord in self._last_ghost_coords:
                 self.input_vec[self._flat_coord(3, ghost_coord)] = 0
@@ -105,21 +105,21 @@ class _OpenBrainInput():
 #CONSTANTS
 #TODO: PUT IN PARAM FILES
 DENSITY = 0.4
-RHO_0 = 3
-                
+RHO_0 = 2
+
 class OpenBrainAgent(Agent):
 
     def __init__(self, evalFn="scoreEvaluation", num_neurons=200 ):
         self.num_inputs = 880 #TODO make sure corrrect.
         self.num_outputs = 4
         self.total_neurons = num_neurons + self.num_inputs + self.num_outputs
-        self.decay_const = 0.5 
-        
+        self.decay_const = 0.5
+
         #init connections
         #make random connections
         self.W = np.random.random((self.total_neurons, self.total_neurons)) > (1 - DENSITY/math.sqrt(num_neurons))
         #give random values to initial weights
-        self.W = self.W*np.random.random((self.total_neurons, self.total_neurons)) 
+        self.W = self.W*np.random.random((self.total_neurons, self.total_neurons))
         self.W *= 1 - np.eye(self.total_neurons) #delete self connections
 
         #Todo gaussian
@@ -132,8 +132,8 @@ class OpenBrainAgent(Agent):
 
         self.threshold =1
         self.rho_reinforce = 10
-        #self.visualize()
-        
+        # self.visualize()
+
         #state vars
         self._input = _OpenBrainInput()
         self._last_state = None
@@ -150,33 +150,34 @@ class OpenBrainAgent(Agent):
         plt.show()
 
     def set_inputs(self, i):
-        self.v[:self.num_inputs] = i #TODO stochastic. 
-    
+        self.v[:self.num_inputs] = i #TODO stochastic.
+
     def get_inputs(self, state):
         self._input.update(state)
         return self._input.get_vec()
 
     def update(self,input_state):
         self.v *= self.decay_const
-        
+
         self.set_inputs(input_state)
-        
-        delta_v = self.W.T.dot(self.R*self.activation(self.v))
-        self.v = self.v - (self.v*self.R)
-        self.v = self.v + delta_v 
+
+        delta_v = self.W.T.dot(self.R * self.activation(self.v))
+        self.v = self.v - (self.v * self.R)
+        self.v = self.v + delta_v
         self.rho = self.rho -1  + self.rho_naught*self.R
         self.R = ((self.rho <= 0)*1) * (( self.v > self.threshold)*1)
 
     def _get_rewards(self, state):
         rewards = {}
         rewards['food'] = self._last_state.getNumFood() - state.getNumFood()
-        
+        rewards['points'] = state.getScore() - self._last_state.getScore()
+
         return rewards
-        
+
     def learn(self,state):
         if self._last_state is None:
             return
-        
+
         rewards = self._get_rewards(state)
 
         #if positive reward
@@ -187,14 +188,30 @@ class OpenBrainAgent(Agent):
                 #need to multiply weights of all connections of all neurons in targets
                 #multiplier is (1 + inverse rho * scale)
                 multiplier = 1 + self.rho_reinforce * np.where(targets, 1./self.rho, 0)
+                self.W = (self.W.T * multiplier).T
+            if name == 'points':
+                if reward > 0:
+                    targets = self.rho > 0
+                    #need to multiply weights of all connections of all neurons in targets
+                    #multiplier is (1 + inverse rho * scale)
+                    multiplier = 1 + self.rho_reinforce * np.where(targets, 1./self.rho, 0)
+                    # print 'multiplier', multiplier
+                    self.W = (self.W.T * multiplier).T
+                else:
+                    targets = self.rho < 0
+                    #need to multiply weights of all connections of all neurons in targets
+                    #multiplier is (1 + inverse rho * scale)
+                    multiplier = 1 - self.rho_reinforce * np.where(targets, 1./self.rho, 0)
+                    # print 'multiplier', multiplier
+                    self.W = (self.W.T * multiplier).T
                 self.W = self.W * multiplier.dot(multiplier.T)
-                
+
     def output_to_action(self, outputs):
         satisfied_neurons = np.argwhere(outputs >= self.threshold)
         n = len(satisfied_neurons)
         if n == 0:
             return Directions.STOP
-        
+
         choice = satisfied_neurons[int(np.random.rand() * n)]
         if choice == 0:
             return Directions.WEST
@@ -204,7 +221,7 @@ class OpenBrainAgent(Agent):
             return Directions.NORTH
         else:
             return Directions.SOUTH
-        
+
     def getAction(self, state):
         incoming_inputs = self.get_inputs(state)
         self.update(incoming_inputs)
