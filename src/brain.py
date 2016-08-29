9b48e7d67460f0c7a72cc5e91d34fb4365210ff9
MadcowD <carlostnb@gmail.com>
2016-08-28 17:12:56 -0700

    Finished Brain error backprop

---
+++ src/brain.py
@@ -0,0 +1,132 @@
+import numpy as np
+import networkx
+import time
+import utils.gnumpy as gpu
+
+from utils.ubigraph import Ubigraph
+
+class Brain(object):
+    """
+    The brain class for openbrain.
+    """
+
+    def __init__(self, num_input, num_hidden, num_output, density=0.5):
+        """
+        Constructs an open brain object.
+        :param num_input: The number of inputs
+        :param num_output: The number of outputs
+        :param num_hidden: The number of hidden neurons.
+        """
+        self.initialize_graph(num_input,num_hidden, num_output,  density=0.5)
+        # self.visualize()
+
+
+    def initialize_graph(self, num_input, num_hidden,  num_output,density):
+        """
+        Initializes the open brain graph.
+        :param num_input:
+        :param num_hidden:
+        :param num_output:
+        :param density:
+        :return:
+        """
+        self.num_neurons = num_input + num_output + num_hidden
+        self.C = np.zeros((self.num_neurons, self.num_neurons))
+
+        adjacency_list = self.define_toplogy(num_input, num_hidden,  num_output, density)
+
+        for i, adjacent in enumerate(adjacency_list):
+            for node in adjacent:
+                self.C[i, node] = np.random.rand(1)
+
+        self.C = gpu.garray(self.C)
+
+    def define_toplogy(self, num_input, num_hidden,  num_output, density):
+        """
+        Defines the topology of the OpenBrain network.
+        :param num_input:
+        :param num_hidden:
+        :param num_output:
+        :param density:
+        :return:
+        """
+        topo = networkx.DiGraph(networkx.watts_strogatz_graph(self.num_neurons, 5, density, seed=None)).to_directed()
+        adjacency_list = topo.adjacency_list()
+
+
+        # Pick the output neurons to be those with highest in degree
+        in_deg = np.array([topo.in_degree(x) for x,_ in enumerate(adjacency_list)])
+        self.output_neurons = np.argpartition(in_deg, -num_output)[-num_output:]
+        print(self.output_neurons)
+        print([topo.in_degree(x) for x in self.output_neurons])
+
+        # Pick the input neurons to be those with highest out degree
+        out_deg = np.array([topo.out_degree(x) if x not in self.output_neurons else -1
+                            for x,_ in enumerate(adjacency_list)])
+        self.input_neurons = np.argpartition(out_deg, -num_input)[-num_input:]
+
+        # Output neurons do not fire out.
+        for adjacent_neurons in adjacency_list:
+            for out_neuron in self.output_neurons:
+                if out_neuron in adjacent_neurons:
+                    adjacent_neurons.remove(out_neuron)
+
+        # Disconnect input -> output
+        for out in self.output_neurons:
+            for inp in self.input_neurons:
+                if out in adjacency_list[inp]: adjacency_list[inp].remove(out)
+                if inp in adjacency_list[out]: adjacency_list[out].remove(inp)
+
+        # Let nothing enter the input neurons
+        for inp in self.input_neurons:
+            adjacency_list[inp] = []
+
+        return adjacency_list
+
+
+    def visualize(self):
+        self.ubi = Ubigraph(self.C)
+        # Color the input and output neurons
+        self.ubi.set_properties({
+            "color": "#1E90FF"
+        }, self.input_neurons)
+
+        self.ubi.set_properties({
+            "color": "#FFD700"
+        }, self.output_neurons)
+
+def get_random_input(b):
+    return gpu.garray(np.array([np.random.rand(1) * 2 - 1.0 if i in b.input_neurons else 0.0 for i in range(b.num_neurons)],
+             dtype=np.float32))
+
+if __name__ == '__main__':
+    b = Brain(2,10,1, 0.2)
+    print(0)
+
+    v =get_random_input(b)
+    i =0
+
+    grad = list()
+    while True:
+        while len(grad) > 200:
+            grad.pop(0)
+
+        i+=1
+        x = get_random_input(b)
+        net = b.C.dot(v + x)
+        dsigma = gpu.diagflat(gpu.tanh(net)*(1 - gpu.tanh(net)))
+        dnet = gpu.garray(np.multiply.outer(np.identity(v.shape[0]), v.as_numpy_array()))
+        dsigmadv = gpu.tensordot(dsigma, gpu.diagflat(v))
+        grad.append(dsigmadv)
+        v = gpu.tanh(net)
+
+        diff = gpu.garray (v.as_numpy_array()[b.output_neurons] - np.array([0.2]))
+        dsigmadC = gpu.tensordot(dsigma, dnet, 1)
+        dpidC = gpu.garray(dsigmadC.as_numpy_array()[b.output_neurons,:,:])
+
+        dEdC = gpu.tensordot(diff, dpidC,1)
+
+        b.C += 0.01*dEdC
+        print(i, v.as_numpy_array()[b.output_neurons])
+
+
---
+++ src/utils/ubigraph.py
@@ -0,0 +1,91 @@
+import sys
+import time
+import xmlrpc.client
+
+def default_node_props():
+    return {
+        'color': "#999999",
+        'shape': "sphere"}
+
+
+class Ubigraph:
+    def __init__(self, adjacency,
+                 server_url='http://127.0.0.1:20738/RPC2',
+                 node_prop=default_node_props()):
+        """
+        Initializes the Ubigraph connection.
+        :param adjacency:
+        :param server_url:
+        :param node_prop:
+        """
+        print("Connecting to UbiGraph at {}".format(server_url))
+
+        server = xmlrpc.client.ServerProxy(server_url)
+        self.G = server.ubigraph
+        self.G.clear()
+
+        print("Connected and cleared.")
+
+        self.nodes = [None for n in range(adjacency.shape[0])]
+        self.edges = []
+        self.node_props = []
+        self.num_edges = 0
+
+        self.build_graph(adjacency, node_prop)
+
+        print("Constructed {} nodes and {} edges."
+              .format(len(self.nodes), self.num_edges))
+
+    def build_graph(self, adjacency, node_property):
+        for r, row in enumerate(adjacency):
+            self.edges += [[None for cell in row]]
+            for c, cell in enumerate(row):
+
+                if not self.nodes[c]:
+                    self.nodes[c] = self.make_node(node_property)
+                if not self.nodes[r]:
+                    self.nodes[r] = self.make_node(node_property)
+
+                if cell != 0:
+                    self.edges[r][c] = self.make_edge(self.nodes[c], self.nodes[r])
+                    self.num_edges += 1
+
+
+    def make_node(self, node_property):
+        """
+        Makes a new vertex.
+        :param node_property: The properties of the vertex.
+        :return:
+        """
+        # Try except because Ubigraph is old as hell!
+        try: n = self.G.new_vertex()
+        except: pass
+        for prop, val in node_property.items():
+            try: self.G.set_vertex_attribute(n, prop, val)
+            except: pass
+        return n
+
+    def make_edge(self, a, b):
+        """
+        Makes an edge from a to be.
+        :param a:
+        :param b:
+        :return:
+        """
+        try: e = self.G.new_edge(a, b)
+        except: pass
+
+        try: self.G.set_edge_attribute(e, "arrow", "true")
+        except: pass
+
+        try: self.G.set_edge_attribute(e, "spline", "false")
+        except: pass
+        return e
+
+    def set_properties(self, props, nodes):
+        for node in nodes:
+            for prop, val in props.items():
+                self.G.set_vertex_attribute(self.nodes[node], prop, val)
+
+
+
