314e44711d531216cac7ab8634f5ec52ebc75118
Noah Golmant <noah.golmant@gmail.com>
2016-10-05 22:14:06 -0700

    add exponential draw for experience replay

--- experiments/exp1/common/replay_buffer.py
+++ experiments/exp1/common/replay_buffer.py
@@ -1,16 +1,24 @@
 from collections import deque
+import numpy as np
 import random
 
 class ReplayBuffer(object):
 
-    def __init__(self, buffer_size):
+    def __init__(self, buffer_size, scale_param = 0.5, uniform = False):
         self.buffer_size = buffer_size
         self.num_experiences = 0
         self.buffer = deque()
 
     def get_batch(self, batch_size):
-        # Randomly sample batch_size examples
-        return random.sample(self.buffer, batch_size)
+        if uniform:
+            return random.sample(self.buffer, batch_size)
+        scale = self.scale_param * len(self.buffer)
+        inds = np.rint(np.random.exponential(scale = scale, size = batch_size))
+        # only go as far back as the start of the deque...
+        inds[inds > len(self.buffer)] = len(self.buffer)
+        # start from the end of the deque
+        inds = -inds
+        return [self.buffer[-int(i)] for i in inds]
 
     def size(self):
         return self.buffer_size
