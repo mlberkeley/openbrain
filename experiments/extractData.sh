374aa6de339dfa94a50a3fa444fdc513170c0472
James Bartlett <jmabartlett2@gmail.com>
2016-10-13 15:12:21 -0700

    Fixed extractData.sh

--- experiments/extractData.sh
+++ experiments/extractData.sh
@@ -3,9 +3,11 @@ if [ -z "$1" ]; then
 	echo usage: $0 logdir
 	exit
 fi
-if [ ! -f serialize_tensorboard.py ]; then
-	wget https://github.com/tensorflow/tensorflow/raw/master/tensorflow/tensorboard/scripts/serialize_tensorboard.py
+if [ -n "$2" ]; then
+	target=$2
+else
+	target=data
 fi
 
 echo logdir $1
-python serialize_tensorboard.py --logdir "$1" --target data --overwrite
+python serialize_tensorboard.py --logdir "$1" --target $target --overwrite
