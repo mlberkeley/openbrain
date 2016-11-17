f345323b969f5c2624f840ade2755d7752996b1e
philkuz <superpker@gmail.com>
2016-11-16 16:35:19 -0800

    Add manual logging that saves log values to csvs.

--- experiments/brain/common/utils.py
+++ experiments/brain/common/utils.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
 import math
+import csv
+import os
 
 TRACK_VARS=False
-
+episode_stats = {'names': [], 'variables' : []}
+output_dir = ''
+cur_episode = -1
+csv_writer = None
 def variable_summaries(var, name):
 	"""Attach a lot of summaries to a Tensor."""
 	with tf.name_scope('summaries'):
@@ -11,16 +16,59 @@ def variable_summaries(var, name):
 		with tf.name_scope('stddev'):
 			stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
 		tf.scalar_summary('stddev/' + name, stddev)
-		tf.scalar_summary('max/' + name, tf.reduce_max(var))
-		tf.scalar_summary('min/' + name, tf.reduce_min(var))
+		stat_max = tf.reduce_max(var)
+		stat_min = tf.reduce_min(var)
+		episode_stats['names'].extend([name+'_mean', name+'_stddev', name+'_max', name+'_min'])
+		episode_stats['variables'].extend([mean, stddev, stat_max, stat_min])
+		tf.scalar_summary('max/' + name, stat_max)
+		tf.scalar_summary('min/' + name, stat_min)
 		tf.histogram_summary(name, var)
+def set_output_dir(directory):
+	global output_dir
+	# make dir if it doesn't exist
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	output_dir = directory
 
+def write_row(episode, timestep, row):
+	"""
+	Writes the current values for episode stats
+	as a new row for the data
+	"""
+	# check what the current file buffer is open
+	global cur_episode, output_dir
+	if cur_episode != episode:
+		cur_episode = episode
+		# TODO test to make sure episode{} doesn't exist
+
+		filename = '{}episode{}.csv'.format(output_dir,episode)
+		if os.path.exists(filename):
+			raise ValueError('{} already exists'.format(filename))
+		with open(filename, 'a+') as f:
+			csv_writer = csv.writer(f)
+			titles = ['timestep'] + episode_stats['names']
+			newrow = [timestep] + row
+			csv_writer.writerow(titles)
+			csv_writer.writerow(newrow)
+
+	else:
+		with open('{}episode{}.csv'.format(output_dir,episode), 'a') as f:
+			csv_writer = csv.writer(f)
+			csv_writer.writerow([timestep] + row)
+
+
+
+
+
+	# open the file buffer
+	# write the line
+	# close the file buffer
 def variable(shape,f, name="Variable"):
-    """
-    Creates a tensor of SHAPE drawn from
-    a random uniform distribution in 0 +/- 1/sqrt(f)
-    """
-    #TODO: fix this. currently shape is a [Dimension, int] object
-    v =  tf.Variable(tf.random_uniform(shape,-1/math.sqrt(f),1/math.sqrt(f)), name=name)
-    if TRACK_VARS: variable_summaries(var, name)
-    return v
+	"""
+	Creates a tensor of SHAPE drawn from
+	a random uniform distribution in 0 +/- 1/sqrt(f)
+	"""
+	#TODO: fix this. currently shape is a [Dimension, int] object
+	v =  tf.Variable(tf.random_uniform(shape,-1/math.sqrt(f),1/math.sqrt(f)), name=name)
+	if TRACK_VARS: variable_summaries(var, name)
+	return v
--- experiments/experiment1.py
+++ experiments/experiment1.py
@@ -17,6 +17,8 @@ gc.enable()
 from brain import DDPG
 from brain import SubCritics
 from brain.common.filter_env import makeFilteredEnv
+import brain.common.utils as utils
+from brain.common.utils import episode_stats, write_row
 
 
 def test(env, agent, num_tests):
@@ -53,7 +55,8 @@ def run_experiment(exp_name, ENV_NAME='LunarLanderContinuous-v2', EPISODES=10000
     train_writer = tf.train.SummaryWriter('/tmp/tboard/{}'.format(exp_name),
                                       agent.sess.graph)
     # To see graph run tensorboard --logdir=/tmp/exp1/tboard
-    agent.sess.run(tf.initialize_all_variables())
+    init_op = tf.initialize_all_variables()
+    agent.sess.run(init_op)
 
     t = 0
     for episode in range(EPISODES):
@@ -61,7 +64,7 @@ def run_experiment(exp_name, ENV_NAME='LunarLanderContinuous-v2', EPISODES=10000
         activations = None
         print("Episode: ", episode, end="")
         r_tot = 0
-
+        old = None # TODO DELETE FOR TESTING
         for step in range(env.spec.timestep_limit):
             t+= 1
             # Explore state space.
@@ -76,7 +79,7 @@ def run_experiment(exp_name, ENV_NAME='LunarLanderContinuous-v2', EPISODES=10000
             if activations is not None and action is not None:
                 ops, feeds = sub_critics.get_perceive_run(activations, next_activations, reward, done)
                 ops += [
-                    agent.critic_network.q_value_output, 
+                    agent.critic_network.q_value_output,
                     agent.critic_network.target_q_value_output]
                 feeds.update({
                     agent.critic_network.state_input: [state],
@@ -86,6 +89,17 @@ def run_experiment(exp_name, ENV_NAME='LunarLanderContinuous-v2', EPISODES=10000
                     })
                 ops = [merged] + ops
                 result = agent.sess.run(ops, feeds)
+                # use ordered dict for stats_map
+                stats_result = agent.sess.run(episode_stats['variables'], feeds)
+                # ## TEST BLOCK TODO DELETE WHEN DONE
+                # test_idx = 100
+                # if old:
+                #
+                #     print(episode_stats['names'][test_idx],stats_result[test_idx] == old, stats_result[test_idx], old)
+                #     exit()
+                # old = stats_result[test_idx]
+                # ## END TEST BLOCK TODO
+                write_row(episode, step, stats_result)
                 train_writer.add_summary(result[0], t)
 
             # Train DDPG
@@ -113,5 +127,23 @@ if __name__ == '__main__':
       the name of the experiment to run.\
       """
     )
+    parser.add_argument(
+        '--output_dir',
+        type=str,
+        default='output/',
+        help="""\
+        The directory to output training csvs\
+        """
+    )
+    parser.add_argument(
+        '--output_csv',
+        dest='output_csv',
+        action='store_true',
+        help="""\
+        Flag to save the experiments as csv\
+        """
+    )
+    parser.set_defaults(output_csv=False)
     args = parser.parse_args()
+    utils.set_output_dir(args.output_dir)
     run_experiment(args.name)
